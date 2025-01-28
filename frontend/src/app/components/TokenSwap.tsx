6a0d64c4c50ff2bc1ff178108b526d0270750f03
Rodrigo Santana Gonçalves <rodrigosg2000@gmail.com>
2025-01-28 20:22:08 -0300

    Disable swap buttons

--- frontend/src/app/components/TokenSwap.tsx
+++ frontend/src/app/components/TokenSwap.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { useBalanceAndRate } from '../hooks/useBalanceAndRate';
 
 export default function TokenSwap() {
+  const { usdcBalance, bltmBalance } = useBalanceAndRate();
   const [amount, setAmount] = useState<string>('');
 
   const handleDeposit = () => {};
@@ -8,6 +10,9 @@ export default function TokenSwap() {
   const handleWithdraw = () => {};
 
   const invalidAmount = isNaN(Number(amount)) || Number(amount) <= 0;
+  const max = Math.max(Number(usdcBalance), Number(bltmBalance));
+  const isDepositDisabled = invalidAmount || Number(amount) > Number(usdcBalance);
+  const isWithdrawDisabled = invalidAmount || Number(amount) > Number(bltmBalance);
 
   return (
     <div className="text-lg font-bold">
@@ -17,26 +22,28 @@ export default function TokenSwap() {
         placeholder="Enter amount"
         value={amount}
         onChange={e => setAmount(e.target.value)}
-        className="border border-gray-300 rounded px-2 py-1"
+        className="border border-gray-300 rounded px-2 py-1 min-w-[48%]"
         style={{ color: 'black' }}
         min={0}
+        max={max}
       />
       <div className="flex justify-between mt-2">
         <button
           onClick={handleDeposit}
           className={
-            'px-4 py-2 bg-green-500 text-white rounded' + (invalidAmount ? ' opacity-50 cursor-not-allowed' : '')
+            'px-4 py-2 bg-green-500 text-white rounded' + (isDepositDisabled ? ' opacity-50 cursor-not-allowed' : '')
           }
-          disabled={invalidAmount}
+          disabled={isDepositDisabled}
         >
           Deposit USDC & Receive BLTM
         </button>
         <button
           onClick={handleWithdraw}
           className={
-            'ml-2 px-4 py-2 bg-red-500 text-white rounded' + (invalidAmount ? ' opacity-50 cursor-not-allowed' : '')
+            'ml-2 px-4 py-2 bg-red-500 text-white rounded' +
+            (isWithdrawDisabled ? ' opacity-50 cursor-not-allowed' : '')
           }
-          disabled={invalidAmount}
+          disabled={isWithdrawDisabled}
         >
           Withdraw BLTM & Receive USDC
         </button>
